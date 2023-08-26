6adbd9665f033a2511252bfe43a3273324f1b259
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2023-08-26 09:07:50 +0530

    fixed some bugs, final commit.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const nav = document.querySelector("#nav");
+const nav = document.querySelector("#nav-main");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
@@ -8,12 +8,28 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// const nav = document.getElementById("nav");
+// const navbar = document.getElementById("nav-main-overlay");
+// let scrolled = false;
 
 // window.addEventListener("scroll", () => {
-//   if (window.scrollY > 0) {
-//     nav.style.backgroundColor = "#fff"; // Change to your desired color
+//   if (window.scrollY > 0) {         //if scrolled
+//     if (!scrolled) {
+//       navbar.style.backgroundColor = "#fff"; // Change to your desired color
+//       scrolled = true;
+//     }
 //   } else {
-//     nav.style.backgroundColor = "rgba(0, 0, 0, 0)";
+//     if (scrolled) {         //if not scrolled
+//       navbar.style.backgroundColor = "rgba(0, 0, 0, 0)";  //transparent
+//       scrolled = false;
+//     }
 //   }
 // });
+
+// This code adds or removes the "overflow-hidden" class based on scroll behavior
+window.addEventListener("scroll", () => {
+  if (window.scrollY > -1) {
+    document.body.classList.add("overflow-hidden");
+  } else {
+    document.body.classList.remove("overflow-hidden");
+  }
+});
